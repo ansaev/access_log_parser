b3d07ce2d743dbfa7f5044c9eedaff1b3e07aa08
Anatolii Saevskii <ansaev@gmail.com>
2016-02-06 17:49:32 +0300

    deliting unnesesary files

--- access_log_parser.py
+++ access_log_parser.py
@@ -2,6 +2,10 @@ import apache_log_parser
 
 
 class AccessLogParser(object):
+    def __init__(self):
+        self.line_parser = None
+        self.access_events = None
+
     def _get_file_content(self, file_name):
         assert file_name
         content = []
@@ -18,10 +22,16 @@ class AccessLogParser(object):
         assert content
         if self.line_parser is None:
             self.line_parser = apache_log_parser.make_parser("%h <<%P>> %t %Dus \"%r\" %>s %b  \"%{Referer}i\" \"%{User-Agent}i\" %l %u")
-        access_events = []
+        self.access_events = []
         for line in content:
             access_event = self.line_parser(line)
-            access_events.append(access_event)
-        return access_events
+            self.access_events.append(access_event)
+        return self.access_events
+
+    def group_by(self, parametr):
+        assert self.access_events
+        assert parametr
+
+
 
 
--- tests.py
+++ tests.py
@@ -3,14 +3,12 @@ from access_log_parser import AccessLogParser
 
 
 class TestClass:
-    @pytest.fixture(params=["<body> <p style='font-size: 12pt'>Example</p></body>"])
+    @pytest.fixture(params=[{"line": '127.0.0.1 <<6113>> [16/Aug/2013:15:45:34 +0000] 1966093us "GET / HTTP/1.1" 200 3478  "https://example.com/" "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.18)" - -', "host": "127.0.0.1"}])
     def file_content(self, request):
         return request.param
 
-    @pytest.fixture(params=["http://habrahabr.ru/some/", "http://www.oracle.com/some/index.html"])
-    def line_content(self, request):
-        return request.param
-
-    def test_style_cleaner(self, page):
-        page = StyleCleanerService._clear_page(page)
-        assert "style=" not in page
+    def test_page_contentParser(self, file_content):
+        parser = AccessLogParser()
+        parsed = parser._parse_file_content([file_content['line']])[0]
+        print(parsed)
+        assert parsed['remote_host'] == file_content['host']
